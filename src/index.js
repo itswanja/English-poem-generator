8710ae0923274762b681fc039181e59fd171c37a
Charity Mwaura <wanjacharity92@gmail.com>
2024-08-12 16:55:57 +0300

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
   // build API Url
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating an English poem about ${instructionInput.value}</div>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`COntext: ${context}`);
